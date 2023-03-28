d1aa98395447ce04b2dbe9c70d3ce4dd6fd493e0
Keagen Thomson <thomsonkeagen@gmail.com>
2023-03-28 00:37:22 -0400

    test question and follow up question

--- src/utils/queryFile.js
+++ src/utils/queryFile.js
@@ -2,6 +2,8 @@ import { HNSWLib } from "langchain/vectorstores";
 import { OpenAIEmbeddings } from "langchain/embeddings";
 import * as path from 'path';
 import { fileURLToPath } from 'url';
+import { OpenAI } from "langchain";
+import { ConversationalRetrievalQAChain } from "langchain/dist/chains";
 
 const currentDir = path.dirname(fileURLToPath(import.meta.url));
 const docsPath = path.resolve(currentDir, '../data/docs');
@@ -9,7 +11,20 @@ const docsPath = path.resolve(currentDir, '../data/docs');
 
 export const run = async () => {
     const vectorStore = await HNSWLib.load(docsPath, new OpenAIEmbeddings());
-
+    const model = new OpenAI({
+        model: "gpt-3.5-turbo",
+        openAIApiKey: process.env.OPENAI_API_KEY,
+    });
+    const chain = ConversationalRetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+    const question = "What are some examples of the Command pattern implemented in Dart instead of C++?";
+    const response = await chain.call({question, chat_history: []});
+    console.log(response);
+    const chatHistory = question + response.text;
+    const followUpResponse = await chain.call({
+        question: "What about those specific examples in Rust instead?",
+        chat_history: chatHistory,
+    });
+    console.log(followUpResponse);
 };
 
 run();
