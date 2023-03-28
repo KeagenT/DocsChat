3ec3251de10e6920c228b3dbb3a417a73546a0d6
Keagen Thomson <thomsonkeagen@gmail.com>
2023-03-27 23:54:51 -0400

    initial commit

---
+++ src/utils/ingestFile.js
@@ -0,0 +1,25 @@
+import { HNSWLib } from "langchain/vectorstores";
+import { OpenAIEmbeddings } from "langchain/embeddings";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import * as fs from "fs";
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const currentDir = path.dirname(fileURLToPath(import.meta.url));
+
+const fileName = "GameDesignPatterns.txt";
+
+const ingestedPath = path.resolve(currentDir, `../data/text/${fileName}`);
+const docsPath = path.resolve(currentDir, '../data/docs');
+
+
+export const run = async () => {
+    const text = fs.readFileSync(ingestedPath, "utf8");
+    const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 100 });
+    const docs = await splitter.createDocuments([text]);
+    const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+    await vectorStore.save(docsPath);
+
+};
+
+run();
