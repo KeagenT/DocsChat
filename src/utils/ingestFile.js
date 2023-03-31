6ddffae8bea06ab1326aea17489e541e79f697fb
Keagen Thomson <thomsonkeagen@gmail.com>
2023-03-31 00:12:54 -0400

    example agent

--- src/utils/ingestFile.js
+++ src/utils/ingestFile.js
@@ -8,9 +8,10 @@ import { fileURLToPath } from 'url';
 const currentDir = path.dirname(fileURLToPath(import.meta.url));
 
 const fileName = "GameDesignPatterns.txt";
+const outputDir = 'dartDocs'
 
 const ingestedPath = path.resolve(currentDir, `../data/text/${fileName}`);
-const docsPath = path.resolve(currentDir, '../data/docs');
+const docsPath = path.resolve(currentDir, `../data/docs/${outputDir}`);
 
 
 export const run = async () => {
--- src/utils/queryFile.js
+++ src/utils/queryFile.js
@@ -1,30 +1,54 @@
 import { HNSWLib } from "langchain/vectorstores";
 import { OpenAIEmbeddings } from "langchain/embeddings";
+import * as fs from "fs";
 import * as path from 'path';
 import { fileURLToPath } from 'url';
-import { OpenAI } from "langchain";
-import { ConversationalRetrievalQAChain } from "langchain/chains";
+import { LLMChain, RetrievalQAChain } from "langchain/chains";
+import { ChatOpenAI } from "langchain/chat_models";
+import { ChainTool } from "langchain/tools";
+import { initializeAgentExecutor } from "langchain/agents";
+import { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate } from "langchain/prompts";
 
 const currentDir = path.dirname(fileURLToPath(import.meta.url));
-const docsPath = path.resolve(currentDir, '../data/docs');
+const docsPath = path.resolve(currentDir, '../data/docs/gameDesignBook');
+const DEFAULT_MODEL = {
+    model: "gpt-3.5-turbo",
+    openAIApiKey: process.env.OPENAI_API_KEY,
+};
 
+const buildModel = () => {
+    return new ChatOpenAI({
+        ...DEFAULT_MODEL
+    });
+}
 
 export const run = async () => {
     const vectorStore = await HNSWLib.load(docsPath, new OpenAIEmbeddings());
-    const model = new OpenAI({
-        model: "gpt-3.5-turbo",
-        openAIApiKey: process.env.OPENAI_API_KEY,
+    const retrievalChain = RetrievalQAChain.fromLLM(buildModel(), vectorStore.asRetriever());
+    const bookTool = new ChainTool({
+        name: 'game-design-patterns-tool',
+        description: 'Game Design Patterns Book QA - used to ask questions about the book Game Design Patterns by Robert Nystrom',
+        chain: retrievalChain,
     });
-    const chain = ConversationalRetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
-    const question = "What are some of the problems the command pattern is most apt to solve?";
-    const response = await chain.call({question, chat_history: []});
-    console.log(response);
-    const chatHistory = question + response.text;
-    const followUpResponse = await chain.call({
-        question: "What benefits does the Command pattern provide?",
-        chat_history: chatHistory,
+    const humanTemplate = "The explanation to convert is as follows: {explanation}";
+    const systemCodingTemplate = "You are a Senior Software Engineer who specializes in Dart. You are able to convert explanations of algorithms or design patterns into Dart code examples.";
+    const codingPrompt = ChatPromptTemplate.fromPromptMessages([
+        SystemMessagePromptTemplate.fromTemplate(systemCodingTemplate),
+        HumanMessagePromptTemplate.fromTemplate(humanTemplate),
+      ]);
+    const codingChain = new LLMChain({ llm: buildModel(), prompt: codingPrompt });
+    const codingTool = new ChainTool({
+        name: 'coding-tool',
+        description: 'Coding Tool - used to generate Dart code examples from input conceptual explanations',
+        chain: codingChain,
     });
-    console.log(followUpResponse);
+
+    
+    const bookExecutor = await initializeAgentExecutor([bookTool, codingTool], buildModel(), "chat-zero-shot-react-description");
+    const input = "How do I conceptually implement the observer pattern based on the game design patterns book? After explaining the concept, please generate a Dart Code example of the pattern in addition to the conceptual explanation.";
+    const result = await bookExecutor.call({input}); 
+    console.log(`${JSON.stringify(result, null, 2)}`);
+
 };
 
 run();
