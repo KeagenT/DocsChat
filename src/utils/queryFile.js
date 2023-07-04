7d378ef48da8923f491a3c8910299949197efa52
Keagen Thomson <thomsonkeagen@gmail.com>
2023-07-04 13:10:33 -0400

    adding an example query

--- src/utils/queryFile.js
+++ src/utils/queryFile.js
@@ -14,7 +14,7 @@ import {
 // Default GPT model to use and OpenAI API key
 // This can be more configurable by loading from a .env file instead of your .bashrc etc file
 const DEFAULT_MODEL = {
-	model: 'gpt-3.5-turbo',
+	model: 'gpt-4',
 	openAIApiKey: process.env.OPENAI_API_KEY
 };
 
@@ -25,7 +25,7 @@ const keyPath = (directoryName) => path.resolve(currentDir, `../data/text/${dire
 
 const buildModel = () => {
 	return new ChatOpenAI({
-		temperature: 0,
+		temperature: 0.2,
 		...DEFAULT_MODEL
 	});
 };
@@ -41,14 +41,34 @@ const retrieverFromDirectory = async (directoryName, results) => {
 }
 
 export const run = async () => {
-	const query = "What's is the difference between a page.server.js and page.js file in SvelteKit?";
-	const retriever = await retrieverFromDirectory('SvelteKit', 5);
+	const query = "What is a Slug in SvelteKit?";
+	const directoryName = 'SvelteKit';
+	const result = await callCodeChain(query, directoryName);
+	console.log(result.text);
+	console.log(result.urls);
+};
+
+async function callCodeChain(query, directoryName) {
+	const retriever = await retrieverFromDirectory(directoryName, 5);
 	const results = await retriever.getRelevantDocuments(query);
 	const metadata = results.map(result => ({'key': result.metadata.key, 'url': result.metadata.url}));
-	const uniqueURLs = [...new Set(metadata.map(item => item.url))];
-	console.log(metadata);
-	console.log(...getContentFromKeys(metadata, 'SvelteKit'))
-};
+	let uniqueURLs = [...new Set(metadata.map(item => item.url))];
+	uniqueURLs = `Sources: ${uniqueURLs.join(', ')};`
+	let relevantDocuments = await getContentFromKeys(metadata, directoryName);
+	relevantDocuments = relevantDocuments.join('\n');
+
+	const SystemTemplate = 'You are a Chatbot who answers user questions about documentation for a given framework or programming language. Please use only the users provided documentation snippets to answer their query.';
+	const HumanTemplate = 'Please answer the following user question: "{query}", using the documentation snippets provided: {relevantDocuments}. Please also include examples in the response if relevant';
+	const codeChainPrompt = ChatPromptTemplate.fromPromptMessages([
+		SystemMessagePromptTemplate.fromTemplate(SystemTemplate),
+		HumanMessagePromptTemplate.fromTemplate(HumanTemplate)
+	]);
+	const codeChain = new LLMChain({llm: buildModel(), prompt: codeChainPrompt});
+	const result = await codeChain.call({query: query, relevantDocuments: relevantDocuments});
+	result.urls = uniqueURLs;
+	return result;
+}
+
 
 const getContentFromKeys = (keys, directory) => {
 	const keyFile = fs.readFileSync(keyPath(directory), 'utf8');
